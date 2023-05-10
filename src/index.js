957391995c9b05ccce6b8006cb4697267c003f53
rakr0819 <raman.krauchanka@netcracker.com>
2023-05-10 21:56:40 +0300

    webpack example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import Post from './Post';
-import WebpackLogoPng from './assets/webpack-logo.png'
+import Post from '@models/Post';
 import './styles/styles.css';
-import json from "./assets/json.json"
+import json from './assets/json';
+import xml from './assets/data.xml';
+import * as $ from 'jquery';
 
-const post = new Post("Post title", WebpackLogoPng);
-console.log("Post to string: ", post.toString());
-console.log("JSON: ", json);
+const post = new Post('Post title');
+
+$('pre').addClass("code").html(post.toString());
+
+console.log('Post to string: ', post.toString());
+console.log('JSON: ', json);
+console.log('XML: ', xml);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   mode: 'development',
-  context: path.resolve(__dirname, "src"),
+  context: path.resolve(__dirname, 'src'),
   entry: {
     main: './index.js',
     analytics: './analytics'
@@ -12,22 +17,56 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  resolve: {
+    alias: {
+      '@models': path.resolve(__dirname, 'src', 'models'),
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  devServer: {
+    port: 4200,
+    hot: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', ]
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          }, 'css-loader',]
       },
       {
-        test: /\.(png|svg|gif|jpg)$/,
-        use: ['file-loader']
-      }
+        test: /\.xml$/,
+        use: ['xml-loader']
+      },
     ]
   }
 }
